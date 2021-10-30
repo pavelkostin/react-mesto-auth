8f178985c772b1d0fc7345d495ad2f649998dd7d
pavelkostin <pasha.kostin@yandex.ru>
2021-10-30 23:05:40 +0300

    12 работа

--- src/components/PopupEditProfile.js
+++ src/components/PopupEditProfile.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useState } from 'react';
 import { Input } from "./Input";
 import { PopupWithForm } from "./PopupWithForm";
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
